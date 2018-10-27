1597f358328f6be9a141c0a8182937edeb78b333
霍晓越 <pino@huoxiaoyuedeMacBook-Pro.local>
2018-10-27 22:52:15 +0800

    2.0.1

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 # coding=utf-8
 from flask import Flask, render_template, url_for, request, session, redirect
-from flask_pymongo import PyMongo
+# from flask_pymongo import PyMongo
 import bcrypt  # to hash the password
 
 app = Flask(__name__)
 
-app.config['MONGO_DBNAME'] = 'web_mongodb'
-app.config['MONGO_URI'] = 'mongodb://hugo:hugohugohugo@ds040877.mlab.com:40877/web_mongodb'
+# app.config['MONGO_DBNAME'] = 'web_mongodb'
+# app.config['MONGO_URI'] = 'mongodb://hugo:hugohugohugo@ds040877.mlab.com:40877/web_mongodb'
 
-mongo = PyMongo(app)
+# mongo = PyMongo(app)
 
 
 @app.route('/')
@@ -22,26 +22,26 @@ def index():
 
 @app.route('/sign_login', methods=['GET', 'POST'])
 def sign_login():
-    if request.method == 'GET':
+    # if request.method == 'GET':
         return render_template('sign_login.html')
 
-    users = mongo.db.users
-    if request.form.get('email_login') is not None:
-        login_user = users.find_one({'email': request.form.get('email_login')})
-        if login_user and bcrypt.hashpw(request.form['password_login'].encode('utf-8'),
-                                        login_user['password']) == login_user['password']:
-            session['email'] = request.form['email_login']
-            return redirect(url_for('index'))
-    else:
-        existing_user = users.find_one({'email': request.form.get('email_signup')})
-        if existing_user is None:
-            hashpass = bcrypt.hashpw(request.form['password_signup'].encode('utf-8'), bcrypt.gensalt())
-            users.insert({'email': request.form['email_signup'], 'password': hashpass,
-                          'username': request.form.get('username_signup'), 'intro': request.form.get('intro_signup')})
-            session['email'] = request.form['email_signup']
-            return redirect(url_for('index'))
-        return 'That email already exists!'
-    return render_template('sign_login.html')
+    # users = mongo.db.users
+    # if request.form.get('email_login') is not None:
+    #     login_user = users.find_one({'email': request.form.get('email_login')})
+    #     if login_user and bcrypt.hashpw(request.form['password_login'].encode('utf-8'),
+    #                                     login_user['password']) == login_user['password']:
+    #         session['email'] = request.form['email_login']
+    #         return redirect(url_for('index'))
+    # else:
+    #     existing_user = users.find_one({'email': request.form.get('email_signup')})
+    #     if existing_user is None:
+    #         hashpass = bcrypt.hashpw(request.form['password_signup'].encode('utf-8'), bcrypt.gensalt())
+    #         users.insert({'email': request.form['email_signup'], 'password': hashpass,
+    #                       'username': request.form.get('username_signup'), 'intro': request.form.get('intro_signup')})
+    #         session['email'] = request.form['email_signup']
+    #         return redirect(url_for('index'))
+    #     return 'That email already exists!'
+    # return render_template('sign_login.html')
 
 
 @app.route('/user_info')
